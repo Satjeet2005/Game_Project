4243f45f5b6a4922112392d67c75d9811f79a3b5
Satjeet Singh <satjeet2005@gmail.com>
2025-07-12 12:34:28 +0530

    add no image functionality

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,11 +1,11 @@
-
+import noImage from "/home/satjeet/Desktop/game-project/src/assets/no-image-placeholder-6f3882e0.webp"
 
 interface Props{
     url : string;
 }
 
 const getImageUrl = ({url} : Props) => {
-    if(!url) return;
+    if(!url) return noImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index)
